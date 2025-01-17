109d0763d776e1936082cc35eb2bf6ff9def3a4b
Oskar Libert <115262767+Mushor00@users.noreply.github.com>
2025-01-17 17:28:18 +0100

    XDDDDDDD

---
+++ kalkulatorTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using prezentacja;
+using prezentacja.classes;
+public class kalkulatorTest
+{
+    private readonly Calculator _calculator = new Calculator();
+
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        Assert.Equal(5, _calculator.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        Assert.Equal(1, _calculator.Subtract(3, 2));
+    }
+
+    [Fact]
+    public void Multiply_ReturnsCorrectProduct()
+    {
+        Assert.Equal(6, _calculator.Multiply(2, 3));
+    }
+
+    [Fact]
+    public void Divide_ReturnsCorrectQuotient()
+    {
+        Assert.Equal(2, _calculator.Divide(6, 3));
+    }
+
+    [Fact]
+    public async Task Divide_ThrowsDivideByZeroException()
+    {
+        await Assert.ThrowsAsync<DivideByZeroException>(async () => await Task.Run(() => _calculator.Divide(1, 0)));
+    }
+}
